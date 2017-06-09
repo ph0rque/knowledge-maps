69dbb0b40d13ffa43179f50a90fcb06ea1a2006b
Andrew Shindyapin <andrei.shindyapin@gmail.com>
2017-06-09 07:04:19 -0400

    work in progress on topic validation

--- knowledge-maps-api/app/models/topic.rb
+++ knowledge-maps-api/app/models/topic.rb
@@ -1,12 +1,16 @@
 class Topic < ApplicationRecord
   has_many :prerequisites, class_name: 'Topic', foreign_key: 'topic_id'
 
-  belongs_to :topic, class_name: 'Topic'
+  belongs_to :topic
   belongs_to :knowledge_map
 
-  validates :topic_cannot_be_its_own_prerequisite
+  validate :topic_cannot_be_its_own_prerequisite
+  validates :classification, inclusion: { in: %w(Subject Milestone),
+    message: "%{value} must be either Subject or Milestone." }
 
   def topic_cannot_be_its_own_prerequisite
-    errors[:base] << 'Topic cannot be its own prerequisite.' if id == topic_id
+    if !topic_id.blank? && id == topic_id
+      errors[:base] << 'Topic cannot be its own prerequisite.'
+    end
   end
 end
