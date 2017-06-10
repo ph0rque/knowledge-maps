b62c6a3d307c52bb392f2172c35ec0ec03c86cf3
Andrew Shindyapin <andrei.shindyapin@gmail.com>
2017-06-10 12:15:24 -0400

    more tweaks; updated model diagrams

--- knowledge-maps-api/app/models/topic.rb
+++ knowledge-maps-api/app/models/topic.rb
@@ -1,12 +1,12 @@
 class Topic < ApplicationRecord
   has_many :prerequisites, class_name: 'Topic', foreign_key: 'topic_id'
-
   belongs_to :dependent_topic, class_name: 'Topic', optional: true
   belongs_to :knowledge_map
 
   validate :topic_cannot_be_its_own_prerequisite
-  validates :classification, inclusion: { in: %w(Subject Milestone),
-    message: "%{value} must be either Subject or Milestone." }
+  validates :classification, inclusion: { in: %w(Subject Milestone), message:
+    "Classification must be either Subject or Milestone (you entered %{value})."
+  }
 
   def topic_cannot_be_its_own_prerequisite
     if !topic_id.blank? && id == topic_id
