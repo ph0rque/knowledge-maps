b914cba86af2a94ac479a248f7265728aeabc101
Andrew Shindyapin <andrei.shindyapin@gmail.com>
2017-06-19 07:36:33 -0400

    a few more tweaks

--- knowledge-maps-api/app/serializers/knowledge_map_serializer.rb
+++ knowledge-maps-api/app/serializers/knowledge_map_serializer.rb
@@ -2,4 +2,11 @@ class KnowledgeMapSerializer < ActiveModel::Serializer
   attributes :name, :description
 
   has_many :topics
+
+  # attribute :links do
+  #   id = object.id
+  #   {
+  #     self: knowledge_map_path(id)
+  #   }
+  # end
 end
